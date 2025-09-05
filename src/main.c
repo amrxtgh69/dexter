34d98a6908001d38004f033d90f342dcbe8072e5
amrxtgh <ghimireamrit619@gmail.com>
2025-09-05 20:23:25 +0545

    make ls command

--- src/main.c
+++ src/main.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
 
-#define MAX_INPUT 1024
+#define MAX_INPUT_SIZE 1024
+#define MAX_ARGS 64
 
+void built_ls(char *path) {
+  DIR *dir;
+  struct dirent *entry;
+  if (path == NULL) path = ".";
+  dir = opendir(path);
+  if (dir == NULL) {
+    perror("ls");
+    return;
+  }
+  while ((entry = readdir(dir)) != NULL) {
+    if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+      printf("%s  ", entry->d_name);
+    }
+ }
+  closedir(dir);
+  printf("\n");
+}
 int main() {
-  char input[MAX_INPUT];
+  char input[MAX_INPUT_SIZE];
   while (1) {
     printf("dexter>  ");
     fflush(stdout);
-    if (fgets(input, MAX_INPUT,  stdin) == NULL) {
+    if (fgets(input, MAX_INPUT_SIZE,  stdin) == NULL) {
       printf("\n");
       break;
     }
@@ -17,7 +36,25 @@ int main() {
     if (strcmp(input, "exit") == 0) {
       break;
     }
-    printf("you typed %s\n", input);
+    char *args[MAX_ARGS];
+    int argc = 0;
+    char *token = strtok(input, " ");
+    while (token != NULL && argc < MAX_ARGS -1) {
+      args[argc++] = token;
+      token = strtok(NULL, " ");
+    }
+    args[argc] = NULL;
+
+    if (argc == 0) {
+    continue;
+    }
+    if (strcmp(args[0], "ls") == 0) {
+      if (argc > 1) {
+        built_ls(args[1]);
+      }
+      else 
+        built_ls(NULL);
+    }
   }
   printf("Exiting the shell\n");
 }
